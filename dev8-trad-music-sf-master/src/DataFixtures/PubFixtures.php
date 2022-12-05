3ec3213168c2f7a501b671206d1ced57fe217dcd
4ndr01 <31137819+4ndr01@users.noreply.github.com>
2022-12-05 09:27:29 +0100

    Rattrapage

--- dev8-trad-music-sf-master/src/DataFixtures/PubFixtures.php
+++ dev8-trad-music-sf-master/src/DataFixtures/PubFixtures.php
@@ -14,23 +14,18 @@ class PubFixtures extends Fixture
         $pub->setAddress('123 Main Street');
         $pub->setCity('London');
         $pub->setPostcode('SW1A 1AA');
-        $pub->setCountry('United Kingdom');
-        $pub->setLatitude(51.507351);
-        $pub->setLongitude(-0.127758);
+        $pub->setImage('https://www.oldpintpot.com/wp-content/uploads/2019/03/old-pint-pot-1.jpg');
         $manager->persist($pub);
         $this->addReference('pub1', $pub);
 
-        $manager->flush();
 
 
         $pub2 = new Pub();
         $pub2->setName('The Old Pint Pot');
         $pub2->setAddress('123 Main Street');
         $pub2->setCity('London');
-        $pub2->setPostcode('SW1A 1AA');
-        $pub2->setCountry('United Kingdom');
-        $pub2->setLatitude(51.507351);
-        $pub2->setLongitude(-0.127758);
+        $pub2->setZipCode('SW1A 1AA');
+        $pub2->setImage('https://www.oldpintpot.com/wp-content/uploads/2019/03/old-pint-pot-1.jpg');
         $manager->persist($pub2);
         $this->addReference('pub2', $pub2);
 
@@ -38,10 +33,13 @@ class PubFixtures extends Fixture
         $pub3->setName('The Old Pint Pot');
         $pub3->setAddress('123 Main Street');
         $pub3->setCity('London');
-        $pub3->setPostcode('SW1A 1AA');
-        $pub3->setCountry('United Kingdom');
+        $pub3->setZipCode('SW1A 1AA');
+        $pub3->setImage('https://www.oldpintpot.com/wp-content/uploads/2019/03/old-pint-pot-1.jpg');
         $manager->persist($pub3);
         $this->addReference('pub3', $pub3);
+
+        $manager->flush();
+
     }
 
 }
